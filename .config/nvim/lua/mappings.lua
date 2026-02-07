ad143eac6cc023835cc1a2d3632059531fcf9df4
Nickolas Zamachnoi <nickzamachnoi@gmail.com>
2026-02-07 17:23:35 -0500

    add back the nav mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,3 +1,5 @@
+-- ~/.config/nvim/lua/mappings.lua
+
 require "nvchad.mappings"
 
 -- add yours here
@@ -13,6 +15,12 @@ del("n", "<C-h>")
 del("n", "<C-j>")
 del("n", "<C-k>")
 del("n", "<C-l>")
+
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { silent = true, desc = "tmux navigate left" })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { silent = true, desc = "tmux navigate down" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { silent = true, desc = "tmux navigate up" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { silent = true, desc = "tmux navigate right" })
+map("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", { silent = true, desc = "tmux navigate previous" })
 
 map({ "n", "v" }, "<leader>gR", "<cmd>Gitsigns reset_hunk<CR>", { desc = "git reset hunk" })
 
