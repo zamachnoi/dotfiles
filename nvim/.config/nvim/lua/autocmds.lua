7395d14785a85b57fd22fa5cf18e93c17c777e79
Nickolas Zamachnoi <nickzamachnoi@gmail.com>
2026-02-08 13:44:29 -0500

    chore(nvim): mv links

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -5,8 +5,10 @@ local auto_reload = vim.api.nvim_create_augroup("auto_reload_file", { clear = tr
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
   group = auto_reload,
   callback = function()
-    if vim.fn.mode() ~= "c" then
-      vim.cmd.checktime()
+    if vim.fn.mode() == "c" or vim.fn.getcmdwintype() ~= "" then
+      return
     end
+
+    vim.cmd.checktime()
   end,
 })
