4608c3eba258783f0bd936139c7854f69617a061
Nickolas Zamachnoi <nickzamachnoi@gmail.com>
2026-02-08 15:10:34 -0500

    chore(nvim): add nvim-tree filewatcher

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -11,29 +11,14 @@ return {
     end,
   },
 
+  -- nvim-tree filewatcher
   {
     "nvim-tree/nvim-tree.lua",
-    opts = function(_, opts)
-      opts.renderer = vim.tbl_deep_extend("force", opts.renderer or {}, {
-        group_empty = true,
-      })
-
-      opts.reload_on_bufenter = true
-
-      opts.filters = vim.tbl_deep_extend("force", opts.filters or {}, {
-        git_ignored = false,
-      })
-
-      opts.actions = vim.tbl_deep_extend("force", opts.actions or {}, {
-        open_file = {
-          window_picker = { enable = false },
-        },
-      })
-
-      opts.filesystem_watchers = vim.tbl_deep_extend("force", opts.filesystem_watchers or {}, {
-        enable = false,
-      })
-      return opts
-    end,
+    opts = {
+      filesystem_watchers = {
+        enable = true,
+        debounce_delay = 50,
+      },
+    },
   },
 }
