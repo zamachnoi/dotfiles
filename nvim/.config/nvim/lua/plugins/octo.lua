e20d0198d285499a40001b9e8a6e249ee434241e
Nickolas Zamachnoi <nickzamachnoi@gmail.com>
2026-02-11 21:59:43 -0500

    Fix Octo picker configuration

--- nvim/.config/nvim/lua/plugins/octo.lua
+++ nvim/.config/nvim/lua/plugins/octo.lua
@@ -7,7 +7,11 @@ return {
       "nvim-telescope/telescope.nvim",
       "nvim-tree/nvim-web-devicons",
     },
-    opts = {},
+    opts = {
+      -- Allow bare :Octo to open the builtin command picker.
+      enable_builtin = true,
+      picker = "telescope",
+    },
     keys = {
       { "<leader>go", "<cmd>Octo<CR>", desc = "octo" },
       { "<leader>gi", "<cmd>Octo issue list<CR>", desc = "github issues" },
